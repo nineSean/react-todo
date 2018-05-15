fc0b3500839f860c0f1f0a5a7f1ddf0114792083
nineSean <ninesean1989@gmail.com>
2018-05-15 18:37:35 +0800

    props

--- src/index.js
+++ src/index.js
@@ -5,5 +5,13 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import Welcome from './Welcome.js'
 
+function More(props){
+  return (
+    <section>
+      <Welcome/>
+      <p>some more things, such as {props.name}</p>
+    </section>
+  )
+}
 
-ReactDOM.render(<Welcome/>, document.querySelector('#root'))
+ReactDOM.render(<More name='apple'/>, document.querySelector('#root'))
