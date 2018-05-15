848baed307a296891fadbba066622b84a1161566
nineSean <ninesean1989@gmail.com>
2018-05-15 18:04:33 +0800

    按需更新

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render( <div>Hello world!</div>, document.getElementById('root'));
-registerServiceWorker();
+function tick(){
+  const element = (
+    <div>
+      <h1>Hello world</h1>
+      <h2>It is {new Date().toLocaleTimeString()}</h2>
+    </div>
+  )
+  ReactDOM.render(element, document.querySelector('#root'))
+}
+setInterval(tick, 1000)
