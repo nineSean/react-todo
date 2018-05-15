605525795200c2ed6033b62b4f3753412a72161b
nineSean <ninesean1989@gmail.com>
2018-05-15 15:07:41 +0800

    some test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import _ from 'lodash'
+import $ from 'jquery'
+import foo from './foo'
+
 function component() {
-  var element = document.createElement('div');
+  var element = $('<div></div>')
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+  element.html(_.join(['hello', 'world'], ' '))
+  return element.get(0);
 }
 
 document.body.appendChild(component());
+foo()
