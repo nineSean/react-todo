2639b90bf6a2b8e3fb9c07160e92713e7b3dc664
nineSean <ninesean1989@gmail.com>
2018-05-15 18:30:51 +0800

    component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Welcome from './Welcome.js'
 
-function tick(){
-  const element = (
-    <div>
-      <h1>Hello world</h1>
-      <h2>It is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  )
-  ReactDOM.render(element, document.querySelector('#root'))
-}
-setInterval(tick, 1000)
+
+ReactDOM.render(<Welcome/>, document.querySelector('#root'))
