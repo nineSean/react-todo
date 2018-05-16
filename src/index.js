3ef4c1c24fdd83fbdf19c4891cab23a58227e3d6
nineSean <ninesean1989@gmail.com>
2018-05-16 17:24:29 +0800

    start todolist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Welcome from './Welcome.js'
 
+class TodoList extends React.Component{
+  constructor(prop){
+    this.state = {
+      items: []
+    }
+  }
+  render(){
+    return (
+      <div>
+        <h1>我的代办事项</h1>
+        <InputBar/>
+        <ItemLIst/>
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(<Welcome/>, document.querySelector('#root'))
+class InputBar extends React.Component{
+  constructor(props){
+
+  }
+  render(){
+    return (
+      <div>
+        <input type="text"/>
+        <button>添加代办事项</button>
+      </div>
+    )
+  }
+}
+class ItemList extends React.Component{
+  constructor(props){
+
+  }
+  render(){
+    return (
+
+    )
+  }
+}
+
+ReactDOM.render(<TodoList/>, document.querySelector('#root'))
