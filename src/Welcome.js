542ae39e7991ba6dd37ddd750543aba4d89e3ae1
nineSean <ninesean1989@gmail.com>
2018-05-16 09:53:26 +0800

    lifecycle

--- src/Welcome.js
+++ src/Welcome.js
@@ -8,9 +8,34 @@ class Welcome extends React.Component{
     setInterval(e => {
       this.setState({date: new Date()})
     }, 1000)
+    console.log('constructor')
+  }
+  componentWillMount(){
+    console.log('componentWillMount')
   }
   render(){
+    console.log('render')
     return <h1>{this.state.date.toLocaleTimeString()}</h1>
+  }
+  componentDidMount(){
+    console.log('componentDidMount')
+  }
+  componentWillReceiveProps(){
+    console.log('componentWillRecieveProps')
+  }
+  shouldComponentUpdate(){
+    console.log('shouldComponentUpdate')
+    return true
+  }
+  componentWillUpdate(){
+    console.log('componentWillUpdate')
+  }
+  componentDidUpdate(){
+    console.log('componentDidUpdate')
+    console.log('------')
+  }
+  componentWillUnmount(){
+    console.log('componentWillUnmount')
   }
 }
 export default Welcome
--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import Welcome from './Welcome.js'
 
-function More(props){
-  return (
-    <section>
-      <Welcome/>
-      <p>some more things, such as {props.name}</p>
-    </section>
-  )
-}
 
 ReactDOM.render(<Welcome/>, document.querySelector('#root'))
